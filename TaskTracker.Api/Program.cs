dd5509d5b053ebe89e315cef1d96c0c173375440
Steven <stevenericborkowski@gmail.com>
2026-04-01 07:49:24 -0400

    updated api program.cs to use OpenAPI and Swagger. also created baisc enpoints

--- TaskTracker.Api/Program.cs
+++ TaskTracker.Api/Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.AspNetCore.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "TaskTracker API v1");
+        options.RoutePrefix = "swagger";
+    });
+}
+
+// Endpoints
+app.MapGet("/", () => Results.Ok("TaskTracker API is running"));
+app.MapGet("/health", () => Results.Ok(new { status = "healthy" }));
 
 app.Run();
+
+public partial class Program { }
