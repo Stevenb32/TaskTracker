f4cde18badb4ffd5169e71ff4c23527b25e32540
Steven <stevenericborkowski@gmail.com>
2026-04-06 12:56:42 -0400

    added TaskTrackerWebApplicationFactory - have not tested if things work yet

--- TaskTracker.Api.Tests/TaskEndpointsTests.cs
+++ TaskTracker.Api.Tests/TaskEndpointsTests.cs
@@ -1,15 +1,20 @@
 using System.Net;
 using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
+using TaskTracker.Api.Dtos;
+using TaskTracker.Domain;
+
 
 namespace TaskTracker.Api.Tests;
 
-public class TaskEndpointsTests : IClassFixture<WebApplicationFactory<Program>>
+public class TaskEndpointsTests : IClassFixture<TaskTrackerWebApplicationFactory>
 {
+    private readonly TaskTrackerWebApplicationFactory _factory;
     private readonly HttpClient _client;
 
-    public TaskEndpointsTests(WebApplicationFactory<Program> factory)
+    public TaskEndpointsTests(TaskTrackerWebApplicationFactory factory)
     {
+        _factory = factory;
         _client = factory.CreateClient();
     }
 
@@ -20,4 +25,69 @@ public class TaskEndpointsTests : IClassFixture<WebApplicationFactory<Program>>
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
+
+
+
+
+
+
+
+
+
+
+    // custom factory tests
+    [Fact]
+    public async Task GetTasks_WhenNoTasksExist_ReturnsOkWithEmptyList()
+    {
+        // Given
+        await _factory.ResetDatabaseAsync();
+
+        // When
+        var response = await _client.GetAsync("/tasks");
+
+        // Then
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var tasks = await response.Content.ReadFromJsonAsync<List<TaskItemResponse>>();
+        tasks.Should().NotBeNull();
+        tasks.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GetTasks_WhenTasksExist_ReturnsOkWithTasks()
+    {
+        // Given
+        await _factory.ResetDatabaseAsync();
+
+        var task = TaskItem.Create(
+            "Buy milk",
+            "From the store",
+            new DateTimeOffset(2026, 3, 25, 7, 0, 0, TimeSpan.Zero));
+
+        await _factory.AddTaskAsync(task);
+
+        // When
+        var response = await _client.GetAsync("/tasks");
+
+        // Then
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var tasks = await response.Content.ReadFromJsonAsync<List<TaskItemResponse>>();
+        tasks.Should().NotBeNull();
+        tasks.Should().HaveCount(1);
+        tasks![0].Title.Should().Be("Buy milk");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
---
+++ TaskTracker.Api.Tests/TaskTrackerWebApplicationFactory.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using TaskTracker.Api.Data;
+using TaskTracker.Domain;
+
+namespace TaskTracker.Api.Tests;
+
+public class TaskTrackerWebApplicationFactory : WebApplicationFactory<Program>
+{
+    private readonly string _databaseName = Guid.NewGuid().ToString();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<TaskTrackerDbContext>));
+
+            services.AddDbContext<TaskTrackerDbContext>(options =>
+            {
+                options.UseInMemoryDatabase(_databaseName);
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            using var scope = serviceProvider.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<TaskTrackerDbContext>();
+
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+        });
+    }
+
+    public async Task ResetDatabaseAsync()
+    {
+        using var scope = Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<TaskTrackerDbContext>();
+
+        await db.Database.EnsureDeletedAsync();
+        await db.Database.EnsureCreatedAsync();
+    }
+
+    public async Task AddTaskAsync(TaskItem task)
+    {
+        using var scope = Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<TaskTrackerDbContext>();
+
+        db.Tasks.Add(task);
+        await db.SaveChangesAsync();
+    }
+}
