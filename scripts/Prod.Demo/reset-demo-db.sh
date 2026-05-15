71a7c0d8df1cf8650b24677a5edf43b217a953af
Steven <stevenericborkowski@gmail.com>
2026-05-15 07:24:25 -0400

    updated demo seed script

--- scripts/Prod.Demo/reset-demo-db.sh
+++ scripts/Prod.Demo/reset-demo-db.sh
@@ -7,7 +7,7 @@ set -a
 source .env
 set +a
 
-echo "Resetting TaskTracker demo database..."
+echo "[$(date)] Resetting TaskTracker demo database..."
 
 docker compose \
   --env-file .env \
@@ -18,4 +18,15 @@ docker compose \
     -d "$TASKTRACKER_POSTGRES_DB" \
     -f /dev/stdin < scripts/Prod.Demo/seed-demo-db.sql
 
-echo "Demo database reset complete."
+echo "[$(date)] Verifying seeded task count..."
+
+docker compose \
+  --env-file .env \
+  -f docker-compose.prod.yml \
+  exec -T tasktracker-db-prod \
+  psql \
+    -U "$TASKTRACKER_POSTGRES_USER" \
+    -d "$TASKTRACKER_POSTGRES_DB" \
+    -c 'SELECT COUNT(*) AS task_count FROM "Tasks";'
+
+echo "[$(date)] Demo database reset complete."
